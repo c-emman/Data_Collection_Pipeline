169f57526ca616812aa6fab910e236ac1d7f5562
Christian <chris.emmanuel@hotmail.com>
2022-07-20 08:41:08 +0100

    Modified the __main__.py file slightly so scraper can run more smoothly as a package.

--- web_scraper/__main__.py
+++ web_scraper/__main__.py
@@ -1,6 +1,7 @@
 from web_scraper.item_scraper import Item_Scraper
+from web_scraper.config import Configuration_XPATH
 
-scrape = Item_Scraper()
+scrape = Item_Scraper(Configuration_XPATH.WEBSITE)
 
 scrape.run_full_scrape()
 
