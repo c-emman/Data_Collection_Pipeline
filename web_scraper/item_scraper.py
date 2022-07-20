2e533d3a5d8f6fc005791f18586494c7b070ee21
Christian <chris.emmanuel@hotmail.com>
2022-07-20 12:37:14 +0100

    Item_Scraper class has been updated so that data already scraped locally and/or on the cloud is not re-scraped.

--- web_scraper/item_scraper.py
+++ web_scraper/item_scraper.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from web_scraper.config import AnyEc, Configuration_XPATH, Db_Config
 from web_scraper.scraper import Scraper
+from sqlalchemy import select
 import pandas as pd
 import argparse
 import sqlalchemy
@@ -25,8 +26,6 @@ class Item_Scraper(Scraper):
         self.list_max = 3
         self.s3_client = boto3.client('s3')
         self.bucketname = 'chris-aircore-s3bucket'
-        self.engine = sqlalchemy.create_engine(f'{Db_Config.DATABASE_TYPE}+{Db_Config.DBAPI}://{Db_Config.USER}:{Db_Config.PASSWORD}@{Db_Config.ENDPOINT}:{Db_Config.PORT}/{Db_Config.DATABASE}')
-        # self.conn = self.engine.connect()
         self.parser = argparse.ArgumentParser(description='Item Scraper class which will scrape products from website.')
         self.parser.add_argument( "-m", "--men", help='Scrape only the Mens department.', default=False, action='store_true')
         self.parser.add_argument("-w", "--women", help='Scrape only the Womens department.', default=False, action='store_true')
@@ -34,6 +33,10 @@ class Item_Scraper(Scraper):
         self.parser.add_argument("-l", "--locally", help='Save scraped data on local machine only.', default=False, action='store_true')
         self.parser.add_argument("-c", "--cloud", help='Upload data to S3 and AWS RDS only.', default=False, action='store_true')
         self.args = self.parser.parse_args()
+        self.flag = False
+        if self.args.locally is False:
+            self.engine = sqlalchemy.create_engine(f'{Db_Config.DATABASE_TYPE}+{Db_Config.DBAPI}://{Db_Config.USER}:{Db_Config.PASSWORD}@{Db_Config.ENDPOINT}:{Db_Config.PORT}/{Db_Config.DATABASE}')
+        self.products_scraped_cloud = list()
         self.dep_list = Configuration_XPATH.DEP_LIST
         if self.args.men is True:
             self.dep_list = Configuration_XPATH.DEP_LIST[0]
@@ -109,8 +112,12 @@ class Item_Scraper(Scraper):
                                             product_info VARCHAR(100),
                                             price FLOAT NOT NULL,
                                             size_and_fit VARCHAR(1000),
-                                            brand_bio VARCHAR(1000)
+                                            brand_bio VARCHAR(2000),
+                                            image_links VARCHAR(1000)
                                             );''')
+                result = self.engine.execute(f'''SELECT product_no FROM {self.department}_data.{self.subcategory}''')
+                for product_no in result:
+                    self.products_scraped_cloud.append(str(product_no).replace("(", "").replace(")", "").replace(",", "").replace("'", ""))
             self.run_item_scrape()
             print(f"{self.subcategory} links in the {self.department}'s department have been retrieved")
 
@@ -126,38 +133,52 @@ class Item_Scraper(Scraper):
             path = Configuration_XPATH.RAW_DATA_PATH + f'/{self.department}/{self.category}/{self.subcategory}'
             self.create_dir(path)
             product_dict = self.scrape_item_data()
-            value_1 = f"'{product_dict['uuid']}'"
-            value_2 = f"'{product_dict['product_no']}'"
-            value_3 = f"'{product_dict['brand']}'"
-            transform_4 = product_dict['product_info'].replace("'s", "s").replace("'", ".")
-            value_4 = f"'{transform_4}'"
-            value_5 = float(product_dict['price'][1:].replace(",", ""))
-            if 'size_and_fit' in product_dict:
-                transform_6 = product_dict['size_and_fit'].replace("'s", "s").replace("'", ".")
-                value_6 = f"'{transform_6}'"
-            else:
-                value_6 = "''"
-            if 'brand_bio' in product_dict:
-                transform_7 = product_dict['brand_bio'].replace("'s", "s").replace("'", ".")
-                value_7 = f"'{transform_7}'"
-            else:
-                value_7 = "''"
-            table_insert = f'({value_1}, {value_2}, {value_3}, {value_4}, {value_5}, {value_6}, {value_7})'
+            if self.flag == True and self.args.locally is False:
+                continue
             item_path = path + f'/{product_dict["product_no"]}'
             self.create_dir(item_path)
             path_img = item_path + '/images'
             self.create_dir(path_img)
-            images_list = self.get_images(path_img, product_dict)
+            images_tuple = self.get_images(path_img, product_dict)
+            images_list = images_tuple[0]
             product_dict["image_links"] = images_list
-            # need to find solution to giving option to save locally or on cloud for json file.encode('utf8')
-            if self.args.cloud is True:
-                self.s3_client.put_object(Body=json.dumps(product_dict), Bucket=self.bucketname, Key=f'{product_dict["product_no"]}.json')
-            if self.args.cloud is False:
-                self.create_json(product_dict, item_path)
-            if self.args.locally is False and self.args.cloud is False:
-                self.upload_data_s3(f'{item_path}/data.json', self.bucketname, f'{product_dict["product_no"]}.json')
+            print(f'items scraped {self.products_scraped_cloud} and is of type: {type(self.products_scraped_cloud)} each element is of type: {type(self.products_scraped_cloud[0])}, {self.products_scraped_cloud[0]}')
             if self.args.locally is False:
-                self.engine.execute(sqlalchemy.text(f'''INSERT INTO {self.department}_data.{self.subcategory}(uuid, product_no, brand, product_info, price, size_and_fit, brand_bio) VALUES{table_insert}'''))   
+                if product_dict['product_no'] in self.products_scraped_cloud:
+                    print(f'Product information for item {product_dict["product_no"]} has already been scraped')
+                    pass
+                else:
+                    value_1 = f"'{product_dict['uuid']}'"
+                    value_2 = f"'{product_dict['product_no']}'"
+                    value_3 = f"'{product_dict['brand']}'"
+                    transform_4 = product_dict['product_info'].replace("'s", "s").replace("'", ".")
+                    value_4 = f"'{transform_4}'"
+                    value_5 = float(product_dict['price'][1:].replace(",", ""))
+                    if 'size_and_fit' in product_dict:
+                        transform_6 = product_dict['size_and_fit'].replace("'s", "s").replace("'", ".")
+                        value_6 = f"'{transform_6}'"
+                    else:
+                        value_6 = "''"
+                    if 'brand_bio' in product_dict:
+                        transform_7 = product_dict['brand_bio'].replace("'s", "s").replace("'", ".")
+                        value_7 = f"'{transform_7}'"
+                    else:
+                        value_7 = "''"
+                    transform_8 = str(images_tuple[1]).replace("'", "*")
+                    value_8 = f"'{transform_8}'"
+                    table_insert = f'({value_1}, {value_2}, {value_3}, {value_4}, {value_5}, {value_6}, {value_7}, {value_8})'
+                    if self.args.cloud is False:
+                        self.create_json(product_dict, item_path)
+                        print(f'...Saving .json file locally for product: {product_dict["product_no"]}...')
+                        self.upload_data_s3(f'{item_path}/data.json', self.bucketname, f'{product_dict["product_no"]}.json')
+                        print(f'...Uploading .json file for product: {product_dict["product_no"]} to S3...')
+                    if self.args.cloud is True:
+                        self.s3_client.put_object(Body=json.dumps(product_dict), Bucket=self.bucketname, Key=f'{product_dict["product_no"]}.json')
+                        print(f'...Uploading .json file for product {product_dict["product_no"]} directly to S3...')
+                    self.engine.execute(sqlalchemy.text(f'''INSERT INTO {self.department}_data.{self.subcategory}(uuid, product_no, brand, product_info, price, size_and_fit, brand_bio, image_links) VALUES{table_insert}'''))
+                    print(f'...Inserting the data into the PostgreSQL AWS RDS database in the {self.department}_data.{self.subcategory} table for product: {product_dict["product_no"]}...')   
+            self.create_json(product_dict, item_path)
+            print(f'...Saving .json file locally for product: {product_dict["product_no"]}...')    
         print(f"All the {self.subcategory} data in the {self.department}'s department has been scraped.")
         
 
@@ -176,33 +197,37 @@ class Item_Scraper(Scraper):
         """
         product_dict = dict()
         WebDriverWait(self.driver, self.delay).until(EC.presence_of_all_elements_located((By.XPATH, Configuration_XPATH.product_no_xpath)))
-        product_dict["uuid"] = str(uuid.uuid4())
-        product_dict["product_no"] = self.driver.find_elements(By.XPATH, Configuration_XPATH.product_no_xpath)[0].text     
-        product_dict["brand"] = self.driver.find_element(By.XPATH, Configuration_XPATH.brand_xpath).text
-        product_dict["product_info"] = self.driver.find_element(By.XPATH, Configuration_XPATH.product_info_xpath).text
-        product_dict["price"] = WebDriverWait(self.driver, self.delay).until(AnyEc(EC.presence_of_element_located((By.XPATH,Configuration_XPATH.price_xpath)), EC.presence_of_element_located((By.XPATH, Configuration_XPATH.price_sale_xpath)))).text
-        self.driver.execute_script("window.scrollTo(0, 500);")
-        if self.driver.find_element(By.XPATH, Configuration_XPATH.HEADING_INFO_ACTIVE_XPATH).text.lower() == "size & fit":
-            product_dict["size_and_fit"] = self.driver.find_element(By.XPATH, Configuration_XPATH.size_and_fit_xpath).text    
+        if self.driver.find_elements(By.XPATH, Configuration_XPATH.product_no_xpath)[0].text in self.products_scraped_cloud:
+            print('Product information has already been scraped')
+            self.flag =  True
         else:
-            try:
-                WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, Configuration_XPATH.SIZE_AND_FIT_INACTIVE_XPATH)))
-                size_and_fit_button = self.driver.find_element(By.XPATH, Configuration_XPATH.SIZE_AND_FIT_INACTIVE_XPATH)
-                size_and_fit_button.click()
-                product_dict["size_and_fit"] = self.driver.find_element(By.XPATH, Configuration_XPATH.size_and_fit_xpath).text
-            except:
-                pass
-        
-        if self.driver.find_element(By.XPATH, Configuration_XPATH.HEADING_INFO_ACTIVE_XPATH).text.lower() == "brand bio":
-            product_dict["brand_bio"] = self.driver.find_element(By.XPATH, Configuration_XPATH.brand_bio_xpath).text   
-        else:
-            try:
-                WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, Configuration_XPATH.BRAND_BIO_INACTIVE_XPATH)))
-                brand_bio_button =  self.driver.find_element(By.XPATH, Configuration_XPATH.BRAND_BIO_INACTIVE_XPATH)
-                brand_bio_button.click()
-                product_dict["brand_bio"] = self.driver.find_element(By.XPATH, Configuration_XPATH.brand_bio_xpath).text
-            except:
-                pass
+            product_dict["uuid"] = str(uuid.uuid4())
+            product_dict["product_no"] = self.driver.find_elements(By.XPATH, Configuration_XPATH.product_no_xpath)[0].text     
+            product_dict["brand"] = self.driver.find_element(By.XPATH, Configuration_XPATH.brand_xpath).text
+            product_dict["product_info"] = self.driver.find_element(By.XPATH, Configuration_XPATH.product_info_xpath).text
+            product_dict["price"] = WebDriverWait(self.driver, self.delay).until(AnyEc(EC.presence_of_element_located((By.XPATH,Configuration_XPATH.price_xpath)), EC.presence_of_element_located((By.XPATH, Configuration_XPATH.price_sale_xpath)))).text
+            self.driver.execute_script("window.scrollTo(0, 500);")
+            if self.driver.find_element(By.XPATH, Configuration_XPATH.HEADING_INFO_ACTIVE_XPATH).text.lower() == "size & fit":
+                product_dict["size_and_fit"] = self.driver.find_element(By.XPATH, Configuration_XPATH.size_and_fit_xpath).text    
+            else:
+                try:
+                    WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, Configuration_XPATH.SIZE_AND_FIT_INACTIVE_XPATH)))
+                    size_and_fit_button = self.driver.find_element(By.XPATH, Configuration_XPATH.SIZE_AND_FIT_INACTIVE_XPATH)
+                    size_and_fit_button.click()
+                    product_dict["size_and_fit"] = self.driver.find_element(By.XPATH, Configuration_XPATH.size_and_fit_xpath).text
+                except:
+                    pass
+            
+            if self.driver.find_element(By.XPATH, Configuration_XPATH.HEADING_INFO_ACTIVE_XPATH).text.lower() == "brand bio":
+                product_dict["brand_bio"] = self.driver.find_element(By.XPATH, Configuration_XPATH.brand_bio_xpath).text   
+            else:
+                try:
+                    WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.XPATH, Configuration_XPATH.BRAND_BIO_INACTIVE_XPATH)))
+                    brand_bio_button =  self.driver.find_element(By.XPATH, Configuration_XPATH.BRAND_BIO_INACTIVE_XPATH)
+                    brand_bio_button.click()
+                    product_dict["brand_bio"] = self.driver.find_element(By.XPATH, Configuration_XPATH.brand_bio_xpath).text
+                except:
+                    pass
 
         return product_dict
       
@@ -218,25 +243,40 @@ class Item_Scraper(Scraper):
             images_list (list): A list of the image links for a specific item.
         """
         images_list = []
+        images_link_list = []
         images_xpath = self.driver.find_elements(By.XPATH, Configuration_XPATH.images_xpath)
 
         a = 1
+        b = 0
         for image in images_xpath:
             image_dict = dict()
+            if self.args.locally is True and os.path.exists(path_img + str(f'/{product_dict["product_no"]}_{a}')):
+                print('Image data has already been saved locally.')
+                continue
             image_dict["image_no"] = str(f'{product_dict["product_no"]}_{a}')
             image_dict["link"] = image.get_attribute('src')
+            images_link_list.append(image.get_attribute('src'))
             img_name = path_img + str(f'/{product_dict["product_no"]}_{a}')
             images_list.append(image_dict)
             if self.args.cloud is False:
-                self.download_images(image_dict["link"], img_name)
-                if self.args.locally is False:
+                if os.path.exists(img_name) == False:
+                    self.download_images(image_dict["link"], img_name)
+                else:
+                    print('Image has already been saved locally')
+                
+                if product_dict["product_no"] in self.products_scraped_cloud:
+                    print('Product image has already been uploaded to S3')
+                else:
                     self.upload_data_s3(f'{img_name}.jpg', self.bucketname, f'{image_dict["image_no"]}.jpg')
+                    b += 1
+                    # print(f'Product: {image_dict["image_no"]} image uploaded to S3')
             else:
                 #add uploading image directly to s3
                 pass
             a+=1
-        
-        return images_list
+        if self.args.locally is False:
+            print(f'{b} images for product: {product_dict["product_no"]} have been uploaded to S3')
+        return images_list, images_link_list
 
     def create_dir(self, PATH: str) -> None:
         """
